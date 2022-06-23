19dda44bf6d34a307badeb16133b8af646d96408
Luis <sans.boba@gmail.com>
2022-06-23 12:38:31 -0700

    restore session created and tested

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,4 +25,14 @@ router.delete('/', (_req, res) => {
   return res.json({ message: 'success' });
 });
 
+// Restore Session User
+router.get('/', restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject()
+    });
+  } else return res.json({});
+});
+
 module.exports = router;
