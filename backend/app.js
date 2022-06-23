77df6e130f121d22cde3b43762451fedb52c45ec
Luis <sans.boba@gmail.com>
2022-06-23 10:04:23 -0700

    server tested and running

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
 // Security Middleware
 if (!isProduction) {
@@ -39,5 +38,7 @@ app.use(
     }
   })
 );
+
+app.use(routes);
 
 module.exports = app;
