2bcae5336c60fe0c5d1ad72375fbd826bdac86a6
Luis <sans.boba@gmail.com>
2022-06-23 15:38:40 -0700

    frontend gets cookies

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+
+const LoginFormPage = () => {
+  return (
+    <div>
+      <form>
+
+      </form>
+    </div>
+  )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
 import reportWebVitals from './reportWebVitals';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+// import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  // window.sessionActions.login = sessionActions.login;
   window.store = store;
 };
 
