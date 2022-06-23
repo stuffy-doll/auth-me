27ba7c44ca47b681a84cd108ed1861499df82eff
Luis <sans.boba@gmail.com>
2022-06-23 12:35:30 -0700

    signup route created and tested w/errors

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
-
+// Signup
+router.post('/', asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await User.signup({ username, email, password });
+  await setTokenCookie(res, user);
+  return res.json({ user });
+}));
 
 module.exports = router;
