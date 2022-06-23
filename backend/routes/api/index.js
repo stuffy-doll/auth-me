8effb0b397fcef0120a007b5aa3bd2f74e2bc905
Luis <sans.boba@gmail.com>
2022-06-23 12:02:03 -0700

    cleared testing code

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,26 +3,6 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
 
-router.get('/restore-user', restoreUser, (req, res) => {
-  return res.json(req.user);
-});
-
-router.get('/require-auth', requireAuth, (req, res) => {
-  return res.json(req.user);
-});
-
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
 module.exports = router;
